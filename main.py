8cf6d4c46515278fed71c8af5fa1e1d4d38a1043
Yoko <yokoxsy@msn.com>
2023-06-29 14:13:33 +0800

    Sftp send function complete.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import math
 import paramiko
+import argparse
 from modules.netcom import AddressOfRegion
 from modules.utilities import download_file, dump_bird
 from constconf import DEFAULTS, RESERVED, RESERVED_V6
 from ipaddress import IPv4Network, IPv6Network
 
+parser = argparse.ArgumentParser(description='Generate non-China routes for BIRD.')
+parser.add_argument('--sshremote', help='SSH to Remote', action="store_true")
+args = parser.parse_args()
 
 address_of_region = AddressOfRegion()
 try:
@@ -44,11 +48,12 @@ with open(DEFAULTS['IPV4CONF'], "w") as fp4:
 with open(DEFAULTS['IPV6CONF'], "w") as fp6:
     dump_bird(address_of_region.root_v6, fp6)
 
-transport = paramiko.Transport((DEFAULTS['HOST'], DEFAULTS['PORT']))  # 获取Transport实例
-transport.connect(username=DEFAULTS['USER'], password=DEFAULTS['PASSWORD'])  # 建立连接
+if args.sshremote:
+    transport = paramiko.Transport((DEFAULTS['HOST'], DEFAULTS['PORT']))  # 获取Transport实例
+    transport.connect(username=DEFAULTS['USER'], password=DEFAULTS['PASSWORD'])  # 建立连接
 
-sftp = paramiko.SFTPClient.from_transport(transport)
-sftp.put(DEFAULTS['IPV4CONF'], "/etc/routes4.conf")
-sftp.put(DEFAULTS['IPV6CONF'], "/etc/routes6.conf")
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    sftp.put(DEFAULTS['IPV4CONF'], "/etc/routes4.conf")
+    sftp.put(DEFAULTS['IPV6CONF'], "/etc/routes6.conf")
 
-transport.close()
+    transport.close()
